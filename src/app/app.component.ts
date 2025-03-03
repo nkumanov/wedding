2236536d274a2cd44015e786a4474bc4fee35a92
nkumanov <kumanov9696@gmail.com>
2025-03-03 16:55:44 +0200

    partial mobile version1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from "./components/intro/intro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
